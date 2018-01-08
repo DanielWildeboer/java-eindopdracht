f6b449feb053e445ae41597a5f4378d999c75dd8
Unknown <d.wildeboer@live.nl>
2018-01-08 16:15:28 +0100

    Api works!

--- backend/src/main/java/api/Application.java
+++ backend/src/main/java/api/Application.java
@@ -1,25 +1,20 @@
 package api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
 
 @RestController
 class Controller {
-
     @RequestMapping("/test")
     String test(){
         return "Hello world";
