edc9195f02e8be9fc1cf460fb903b3ee3df88286
Unknown <d.wildeboer@live.nl>
2018-01-08 12:26:43 +0100

    api test

---
+++ backend/src/main/java/api/Main.java
@@ -0,0 +1,15 @@
+package api;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Main {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "Hello world";
+    }
+}
