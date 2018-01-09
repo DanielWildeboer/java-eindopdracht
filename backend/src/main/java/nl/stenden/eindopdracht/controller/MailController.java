0b81278ee4ce0305aebd085e4ddab7cbaa78b881
Lesley Jordan van Oostenrijk <ljvanoostenrijk@live.nl>
2018-01-09 13:41:08 +0100

    Mailer is klaar, nu een beveiligingsdingetje zoeken en implementeren
    mailer is klaar en werkt

---
+++ backend/src/main/java/nl/stenden/eindopdracht/controller/MailController.java
@@ -0,0 +1,48 @@
+package nl.stenden.eindopdracht.controller;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Message.RecipientType;
+
+public class MailController {
+
+    public static void main(String[] args) {
+        final String username = "javatestlesley@gmail.com";
+        final String password = "lesley123";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host","smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+
+
+        Session session = Session.getInstance(props,
+            new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("iemand@gmail.com"));
+
+
+            message.setRecipients(RecipientType.TO, InternetAddress.parse("lesley.van.oostenrijk@student.stenden.com"));
+            message.setSubject("Een test mail");
+            message.setText("Beste Student," +
+                "\n\n bezoek de onderstaande link om je medegroepleden te beoordelen" +
+                "\n\n Met vriendelijke groet," +
+                "\n\n Rob Smit");
+
+            Transport.send(message);
+            System.out.println("message sent");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
