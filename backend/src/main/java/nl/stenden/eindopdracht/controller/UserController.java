364fb2d35b6a30bf2293c44dacd12d7b4a8b8c24
Unknown <d.wildeboer@live.nl>
2018-01-10 15:19:42 +0100

    Registration works kinda?

--- backend/src/main/java/nl/stenden/eindopdracht/controller/UserController.java
+++ backend/src/main/java/nl/stenden/eindopdracht/controller/UserController.java
@@ -25,21 +25,17 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
-    @RequestMapping(value = "/registration", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity registration(@RequestBody User userForm, BindingResult bindingResult){
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ResponseEntity registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult){
         HttpHeaders headers = new HttpHeaders();
-        try{
+
             userValidator.validate(userForm, bindingResult);
             if(bindingResult.hasErrors()){
                 return new ResponseEntity<String>(bindingResult.getAllErrors().toString(),
                         headers, HttpStatus.INTERNAL_SERVER_ERROR);
             }
             userService.save(userForm);
-        } catch(Exception e) {
 
-            return new ResponseEntity<String>(e.toString(),
-                    headers, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
         return new ResponseEntity<User>(userForm, HttpStatus.CREATED);
     }
 
