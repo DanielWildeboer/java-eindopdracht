2b3846874a0d85df84fda560f68bcf1b2de44c03
Ramon Valk <valkramon1@gmail.com>
2018-01-12 11:23:18 +0100

    Even de groepjes updaten

--- frontend/src/groups/script.js
+++ frontend/src/groups/script.js
@@ -3,12 +3,12 @@ var app = angular.module('mainApp', []);
 app.controller('studentsController', function($scope) {
 
 	$scope.groups = [
-		{name: "Team 1", status: "Compleet" },
-		{name: "Team 2", status: "Open" },
-		{name: "Team 3", status: "Open" },
-		{name: "Team 420", status: "Dank" },
-		{name: "Team 5", status: "Compleet" },
-		{name: "Team 6", status: "Compleet" },
-		{name: "Team 7", status: "Compleet" },
+		{name: "Team 1", subject: "Een kanker taart bakken", status: "Compleet" },
+		{name: "Team 2", subject: "CEH (alweer)", status: "Open" },
+		{name: "Team 3", subject: "Java Minor", status: "Open" },
+		{name: "Team 420", subject: "JWZ", status: "Dank" },
+		{name: "Team 5", subject: "Java Minor", status: "Compleet" },
+		{name: "Team 6", subject: "Java Minor", status: "Compleet" },
+		{name: "Team 7", subject: "Java Minor", status: "Compleet" },
 	]
 });
