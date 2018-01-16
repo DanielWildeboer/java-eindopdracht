ad5cf7265d495363eafbe6c4f678cc9e1216b883
Lesley Jordan van Oostenrijk <ljvanoostenrijk@live.nl>
2018-01-16 14:25:00 +0100

    Auto stash before merge of "master" and "origin/master"

--- backend/src/main/java/nl/stenden/eindopdracht/service/UserService.java
+++ backend/src/main/java/nl/stenden/eindopdracht/service/UserService.java
@@ -2,8 +2,16 @@ package nl.stenden.eindopdracht.service;
 
 import nl.stenden.eindopdracht.model.User;
 
+import java.util.List;
+
 public interface UserService {
     void save(User user);
 
+    List<User> findAll();
+
     User findByEmail(String email);
+
+    User findById(int Id);
+
+    void delete(int id);
 }
