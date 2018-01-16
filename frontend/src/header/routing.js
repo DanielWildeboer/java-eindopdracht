e8e46d3b5b72d6d60722641bd04372747ade23e7
Ramon Valk <valkramon1@gmail.com>
2018-01-16 16:40:28 +0100

    Dashboard is redelijk ready voor api & fab is terug in de table

--- frontend/src/header/routing.js
+++ frontend/src/header/routing.js
@@ -1,20 +1,34 @@
 var app = angular.module('mainApp', ['ngRoute']);
 
-app.controller('headerController', function($scope) {
+app.controller('headerController', function($scope, $route) {
+    $scope.$route = $route;
+
     $scope.date = new Date();
+    $scope.dashName = 'Rob Smit';
+
+    $scope.acronym = function(name) {
+
+        var matches = name.match(/\b(\w)/g);
+        var acronym = matches.join('');
+
+        return acronym
+    };
 });
 
 
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'src/card/card.html'
+            templateUrl: 'src/card/card.html',
+            activeTab: 'dashboard'
         })
         .when('/groups', {
-            templateUrl: 'src/groups/groups.html'
+            templateUrl: 'src/groups/groups.html',
+            activeTab: 'groups'
         })
         .when('/addGroup', {
-            templateUrl: 'src/groups/group-detail/group-detail.html'
+            templateUrl: 'src/groups/group-detail/group-detail.html',
+            activeTab: 'groups'
         })
 
 });
