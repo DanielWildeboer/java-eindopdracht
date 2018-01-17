3220ad55f4edf4ff34314b3567ddd3d628899da8
Pim Gelmers <p.gelmers@live.nl>
2018-01-17 11:14:45 +0100

    Repository for Token

---
+++ backend/src/main/java/nl/stenden/eindopdracht/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package nl.stenden.eindopdracht.repository;
+
+import nl.stenden.eindopdracht.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("tokenRepository")
+public interface TokenRepository extends JpaRepository<Token, Long>{
+    Token findByGroupIdAndStudentId(String groupId, String studentId);
+}
