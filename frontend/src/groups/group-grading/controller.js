3d39ce7cd5537c787bfe7ac79ce8faf82c56afea
Ramon Valk <valkramon1@gmail.com>
2018-01-18 18:05:35 +0100

    grading done

--- frontend/src/groups/group-grading/controller.js
+++ frontend/src/groups/group-grading/controller.js
@@ -1,87 +1,57 @@
 app.controller('gradingController', function ($scope, $route) {
     $scope.group = {
         name: "Team Rood", subject: "Java Minor", groupGrade: 7.4, status: "Gesloten", members: [
-            {
-                name: "Bjorne Hoeksema", grade: 7.0, feedback: [
-                {name: "Bjorne Hoeksema", grade: 7.1, comment: "1Feedback bla bla bla redenen bla bla bla Stenden."},
+            {name: "Bjorne Hoeksema", grade: 2.0, feedback: [
+                {name: "Bjorne Hoeksema", grade: 2.1, comment: "1Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Pim Gelmers", grade: 7.2, comment: "2Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Rik Hassing", grade: 7.3, comment: "3Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Ramon Valk", grade: 7.4, comment: "4Feedback bla bla bla redenen bla bla bla Stenden."},
-                {
-                    name: "Lesley van Oostenrijk",
-                    grade: 7.5,
-                    comment: "5Feedback bla bla bla redenen bla bla bla Stenden."
-                },
+                {name: "Lesley van Oostenrijk", grade: 7.5, comment: "5Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Daniel Wildeboer", grade: 7.6, comment: "6Feedback bla bla bla redenen bla bla bla Stenden."}
             ]
             },
-            {
-                name: "Pim Gelmers", grade: 7.4, feedback: [
+            {name: "Pim Gelmers", grade: 7.4, feedback: [
                 {name: "Bjorne Hoeksema", grade: 7.4, comment: "7Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Pim Gelmers", grade: 7.4, comment: "8Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Rik Hassing", grade: 7.4, comment: "9Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Ramon Valk", grade: 7.4, comment: "10Feedback bla bla bla redenen bla bla bla Stenden."},
-                {
-                    name: "Lesley van Oostenrijk",
-                    grade: 7.4,
-                    comment: "11Feedback bla bla bla redenen bla bla bla Stenden."
-                },
+                {name: "Lesley van Oostenrijk", grade: 7.4, comment: "11Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Daniel Wildeboer", grade: 7.4, comment: "12Feedback bla bla bla redenen bla bla bla Stenden."}
             ]
             },
-            {
-                name: "Rik Hassing", grade: 7.4, feedback: [
+            {name: "Rik Hassing", grade: 7.4, feedback: [
                 {name: "Bjorne Hoeksema", grade: 7.4, comment: "13Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Pim Gelmers", grade: 7.4, comment: "14Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Rik Hassing", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Ramon Valk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {
-                    name: "Lesley van Oostenrijk",
-                    grade: 7.4,
-                    comment: "Feedback bla bla bla redenen bla bla bla Stenden."
-                },
+                {name: "Lesley van Oostenrijk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Daniel Wildeboer", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."}
             ]
             },
-            {
-                name: "Ramon Valk", grade: 7.4, feedback: [
+            {name: "Ramon Valk", grade: 7.4, feedback: [
+                {name: "Bjorne Hoeksema", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
+                {name: "Pim Gelmers", grade: 1.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
+                {name: "Rik Hassing", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
+                {name: "Ramon Valk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
+                {name: "Lesley van Oostenrijk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
+                {name: "Daniel Wildeboer", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."}
+            ]
+            },
+            {name: "Lesley van Oostenrijk", grade: 8.0, feedback: [
                 {name: "Bjorne Hoeksema", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Pim Gelmers", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Rik Hassing", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Ramon Valk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {
-                    name: "Lesley van Oostenrijk",
-                    grade: 7.4,
-                    comment: "Feedback bla bla bla redenen bla bla bla Stenden."
-                },
+                {name: "Lesley van Oostenrijk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Daniel Wildeboer", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."}
             ]
             },
-            {
-                name: "Lesley van Oostenrijk", grade: 8.0, feedback: [
+            {name: "Daniel Wildeboer", grade: 7.4, feedback: [
                 {name: "Bjorne Hoeksema", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Pim Gelmers", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Rik Hassing", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Ramon Valk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {
-                    name: "Lesley van Oostenrijk",
-                    grade: 7.4,
-                    comment: "Feedback bla bla bla redenen bla bla bla Stenden."
-                },
-                {name: "Daniel Wildeboer", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."}
-            ]
-            },
-            {
-                name: "Daniel Wildeboer", grade: 7.4, feedback: [
-                {name: "Bjorne Hoeksema", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {name: "Pim Gelmers", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {name: "Rik Hassing", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {name: "Ramon Valk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
-                {
-                    name: "Lesley van Oostenrijk",
-                    grade: 7.4,
-                    comment: "Feedback bla bla bla redenen bla bla bla Stenden."
-                },
+                {name: "Lesley van Oostenrijk", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."},
                 {name: "Daniel Wildeboer", grade: 7.4, comment: "Feedback bla bla bla redenen bla bla bla Stenden."}
             ]
             }
@@ -97,7 +67,7 @@ app.controller('gradingController', function ($scope, $route) {
             angular.forEach(gradeArray, function (value) {
                 sum += parseFloat(value);
             });
-            return (sum / gradeArray.length);
+            return (Math.round((sum / gradeArray.length) * 10) / 10).toFixed(1);
     };
 });
 
