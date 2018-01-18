c0fb54c7338963713488ee131230f4109b6e70cc
Pim Gelmers <p.gelmers@live.nl>
2018-01-18 15:53:04 +0100

    Now building url for assesment

--- backend/src/main/java/nl/stenden/eindopdracht/controller/MailController.java
+++ backend/src/main/java/nl/stenden/eindopdracht/controller/MailController.java
@@ -3,6 +3,7 @@ package nl.stenden.eindopdracht.controller;
 import nl.stenden.eindopdracht.model.Email;
 import nl.stenden.eindopdracht.model.ProjectGroup;
 import nl.stenden.eindopdracht.model.Student;
+import nl.stenden.eindopdracht.model.Token;
 import nl.stenden.eindopdracht.service.EmailService;
 import nl.stenden.eindopdracht.service.GroupService;
 import nl.stenden.eindopdracht.service.TokenService;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -29,22 +31,17 @@ public class MailController {
 
     //GET ALL GROUPS
     @RequestMapping(value = "/sentMail/{toEmail}/{fromEmail}/{tokenId}", method = RequestMethod.POST)
-    public void sentMail(@PathVariable String toEmail, @PathVariable String fromEmail, @PathVariable int tokenId){
-            tokenService.findTokenByIds(tokenId);
-            Email email = new Email(fromEmail, toEmail, "Beoordeling", "Beste Student," +
-                    "\n\n bezoek de onderstaande link om je medegroepleden te beoordelen" +
-                    "\n\n bezoek de onderstaande link om je medegroepleden te beoordelen" +
-                    "\n\n Met vriendelijke groet");
-            emailService.sendEmail(email);
+    public void sentMail(@PathVariable String toEmail, @PathVariable String fromEmail, @PathVariable int tokenId) {
+        Token token = tokenService.findTokenByIds(tokenId);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://localhost:8080/beoordeling");
 
-    }
+        builder.queryParam("studentId", token.getStudentId());
+        builder.queryParam("groupId", token.getGroupId());
+        builder.queryParam("token", token.getRandomString());
 
-    //GET ALL GROUPS
-    @RequestMapping(value = "/sentMail", method = RequestMethod.POST)
-    public void sentMail(){
-        Email email = new Email("p.gelmers@live.nl", "p.gelmers@live.nl", "Beoordeling", "Beste Student," +
+        Email email = new Email(fromEmail, toEmail, "Beoordeling", "Beste Student," +
                 "\n\n bezoek de onderstaande link om je medegroepleden te beoordelen" +
-                "\n\n bezoek de onderstaande link om je medegroepleden te beoordelen" +
+                "\n\n" + builder.build().toString() +
                 "\n\n Met vriendelijke groet");
         emailService.sendEmail(email);
 
