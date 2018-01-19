48a9bca0ffab2c3ef4e62e0052abbce15da0019d
Bjorne Hoeksema <bjorne.hoeksema@student.stenden.com>
2018-01-19 13:06:01 +0100

    idk man

--- frontend/src/Login/login.js
+++ frontend/src/Login/login.js
@@ -8,20 +8,21 @@ app.controller('LoginController', ['$scope', 'LoginService', '$localStorage', '$
                 function (errorMessage) {
                     console.warn(errorMessage);
                 }
-            )
-        // new LoginService({email: $scope.email, password: $scope.password},
-        //
-        // function (data, headers) {
-        //         $localStorage.user = data.user;
-        //         $localStorage.authToken = headers['x-auth-token'];
-        //         $http.defaults.headers.common['x-auth-token'] = headers['x-auth-token'];
-        //     }, function (error) {
-        //         console.log(error);
-        //     });
+            );
+        new LoginService({email: $scope.email, password: $scope.password},
+
+        function (data, headers) {
+                $localStorage.user = data.user;
+                $localStorage.authToken = headers['x-auth-token'];
+                $http.defaults.headers.common['x-auth-token'] = headers['x-auth-token'];
+            }, function (error) {
+                console.log(error);
+            });
     };
 }]);
 
 app.service('LoginService', function ($http, $q) {
+
 
     return ({
         login: login
@@ -30,16 +31,15 @@ app.service('LoginService', function ($http, $q) {
     function login(email, password) {
         var formData = new FormData();
 
-        formData.append('email', email);
-        formData.append('password', password);
-
         var request = $http({
             method: "post",
             url: "http://127.0.0.1:8080/api/login",
-            data: formData,
+            data: {
+                email: email,
+                password: password
+            },
             headers: {
-                'Content-Type': 'multipart/form-data',
-                'Accept': 'multipart/form-data'
+                'Content-Type': 'application/json'
             }
         });
 
