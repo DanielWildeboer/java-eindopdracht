6b0cba9e2afcd6d34ac7fb68aae6d4d33a5daa1a
Pim Gelmers <p.gelmers@live.nl>
2018-01-19 12:00:40 +0100

    GradeAssessment

--- backend/src/main/java/nl/stenden/eindopdracht/repository/GradeAssessmentRepository.java
+++ backend/src/main/java/nl/stenden/eindopdracht/repository/GradeAssessmentRepository.java
@@ -1,5 +1,6 @@
 package nl.stenden.eindopdracht.repository;
 
+import nl.stenden.eindopdracht.model.GradeAssessment;
 import nl.stenden.eindopdracht.model.ProjectGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Set;
 
 @Repository("GradeAssessmentRepository")
-public interface GradeAssessmentRepository extends JpaRepository<ProjectGroup, Integer> {
+public interface GradeAssessmentRepository extends JpaRepository<GradeAssessment, Integer> {
+    GradeAssessment findByGroup_idAndStudent_id(String group_id, String student_id);
 }
--- backend/src/main/java/nl/stenden/eindopdracht/service/GradeAssessmentService.java
+++ backend/src/main/java/nl/stenden/eindopdracht/service/GradeAssessmentService.java
@@ -1,7 +1,12 @@
 package nl.stenden.eindopdracht.service;
 
-/**
- * Created by Gebruikersnaam on 19-1-2018.
- */
-public class GradeAssessmentService {
+import nl.stenden.eindopdracht.model.GradeAssessment;
+
+import java.util.Set;
+
+public interface GradeAssessmentService {
+    GradeAssessment findGradeAssessmentsByGroupIdAndStudentId(String groupId, String studentId);
+    void addGradeAssessment(GradeAssessment gradeAssessment);
 }
+
+
