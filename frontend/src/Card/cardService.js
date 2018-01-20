f10bece23e90a144901c0991cda51ba59e006dbc
Pim Gelmers <p.gelmers@live.nl>
2018-01-20 11:23:18 +0100

    Added cardService

---
+++ frontend/src/Card/cardService.js
@@ -0,0 +1,38 @@
+app.service('cardService', ['$http', function($scope, $http) {
+
+    return ({
+        getGroups : getGroups,
+        getStudents : getStudents
+    });
+
+    function getGroups(userId) {
+        var request = $http({
+            method: "get",
+            url: "http://127.0.0.1:8080/api/group/" + userId
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function getStudents() {
+        var request = $http({
+            method: "get",
+            url: "http://127.0.0.1:8080/api/student"
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function handleError(response) {
+        if (
+            !angular.isObject(response.data) ||
+            !response.data.message
+        ) {
+            return ( $q.reject("An unknown error occurred.") );
+        }
+        return ( $q.reject(response.data.message) );
+    }
+
+    function handleSuccess(response) {
+        return ( response.data );
+    }
+
+}]);
