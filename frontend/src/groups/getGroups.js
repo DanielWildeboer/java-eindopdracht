7a5c9c14192bf8543b2650650e726faecd719d9a
Bjorne Hoeksema <bjorne.hoeksema@student.stenden.com>
2018-01-20 10:39:15 +0100

    Update

--- frontend/src/groups/getGroups.js
+++ frontend/src/groups/getGroups.js
@@ -1,12 +1,19 @@
-app.service('getGroups', ['$http', function($http) {
-    this.get = function (id) {
-    	var url = "/api/user/group/" + id;
-        $http.get(url)
-        			.then(function (response){
-						return response.data;
-						console.log("Got the data");
-					}).catch(function(response) {
-					  console.error('Error occurred:', response.status, response.data);
-					});
+app.service('getGroups', ['$http', function($scope, $http) {
+
+
+	$scope.data = [];
+
+    function loadData() {
+        $http.get("http://127.0.0.1:8080/api/group")
+            .then(function (response) {
+                $scope.data = response;
+                console.log($scope.data);
+            })
+            .catch(function(error) {
+                $scope.error = error;
+            });
     }
+
+    loadData();
+
 }]);
