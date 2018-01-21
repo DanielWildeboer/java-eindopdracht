6c322c694280d90c48bbf8035ab0e46e4f7436ba
Pim Gelmers <p.gelmers@live.nl>
2018-01-21 23:38:46 +0100

    Assessing works

--- frontend/src/grading/gradingController.js
+++ frontend/src/grading/gradingController.js
@@ -1,15 +1,29 @@
 var app = angular.module('gradeApp', ['ngRoute', 'ngMaterial']);
 app.controller('gradingController', function ($scope, $location, gradingService) {
-    gradingService.getGradeAssessment(0)
+    $scope.userId = 1;
+    $scope.groupId = getParameterByName('groupId');
+    $scope.studentId = getParameterByName('studentId');
+
+    gradingService.getGradeAssessment($scope.groupId)
         .then(function (response) {
             $scope.gradeAssessment = response;
-            console.log($scope.gradeAssessment)
         });
 
     $scope.addGradingAssessment = function() {
         angular.forEach($scope.gradeAssessment.students, function(student, key) {
             console.log(student);
-            gradingService.addGradingAssessment(0, student.id, 1, 3, student.id, student.description, student.grade)
+            gradingService.addGradingAssessment($scope.groupId, student.id, $scope.userId, $scope.studentId, student.id, student.description, student.grade)
         });
+    };
+
+    // Because it actually is a separate application and so $routeParams won't work
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 });
