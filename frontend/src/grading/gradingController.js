87fb3f8c84ae1c2bff702ba1a338327c6f92b318
Pim Gelmers <p.gelmers@live.nl>
2018-01-21 20:34:12 +0100

    Auto stash before merge of "master" and "origin/master"

--- frontend/src/grading/gradingController.js
+++ frontend/src/grading/gradingController.js
@@ -1,6 +1,8 @@
 var app = angular.module('gradeApp', ['ngRoute', 'ngMaterial']);
-app.controller('gradingController', function($scope, $location) {
-	$scope.grades = {grade: "7.0", names: [ "Bjorne Hoeksema", "Lesley van Oostenrijk", "Pim Gelmers", "Daniel Wildeboer", "Rik Hassing", "Ramon Valk"]};
-
-	
+app.controller('gradingController', function ($scope, $location, gradingService) {
+    $scope.grades = {
+        grade: "7.0",
+        names: ["Bjorne Hoeksema", "Lesley van Oostenrijk", "Pim Gelmers", "Daniel Wildeboer", "Rik Hassing", "Ramon Valk"]
+    };
+    console.log(gradingService.getGradeAssessment())
 });
--- frontend/src/grading/gradingService.js
+++ frontend/src/grading/gradingService.js
@@ -1,39 +1,18 @@
-app.service('cardService', ['$http', function($scope, $http) {
+app.service('gradingService', function ($http) {
 
     return ({
-        getGradeAssessment : getGradeAssessment,
-        addGradeAssessment : addGradeAssessment
+        getGradeAssessment: getGradeAssessment,
     });
 
-    function getGradeAssessment(groupId) {
-        var request = $http({
-            method: "get",
-            url: "http://127.0.0.1:8080/api/group/" + groupId
+    function getGradeAssessment() {
+        console.log("getGradeAssessment");
+        $http({
+            method: "GET",
+            url: "http://127.0.0.1:8080/api/group/0"
+        }).then(function(response){
+            console.log(response.data);
+            return response.data;
         });
-        return (request.then(handleSuccess, handleError));
     }
-
-    function addGradeAssessment() {
-        var request = $http({
-            method: "get",
-            url: "http://127.0.0.1:8080/api/student"
-        });
-        return (request.then(handleSuccess, handleError));
-    }
-
-    function handleError(response) {
-        if (
-            !angular.isObject(response.data) ||
-            !response.data.message
-        ) {
-            return ( $q.reject("An unknown error occurred.") );
-        }
-        return ( $q.reject(response.data.message) );
-    }
-
-    function handleSuccess(response) {
-        return ( response.data );
-    }
-
-}]);
+});
 
