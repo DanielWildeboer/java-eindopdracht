2226fae87b5ee21041cc1289d13abbc3cf35d81d
Pim Gelmers <p.gelmers@live.nl>
2018-01-21 19:13:46 +0100

    RequestMapping for adding a gradeAssessment

--- backend/src/main/java/nl/stenden/eindopdracht/controller/GradeAssessmentController.java
+++ backend/src/main/java/nl/stenden/eindopdracht/controller/GradeAssessmentController.java
@@ -5,10 +5,7 @@ import nl.stenden.eindopdracht.service.GradeAssessmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -25,4 +22,10 @@ public class GradeAssessmentController {
         return new ResponseEntity<>(gradeAssessment, HttpStatus.OK);
     }
 
+    //ADD GRADEASSESSMENT
+    @RequestMapping(value = "api/grade/assessment}", method = RequestMethod.POST)
+    public ResponseEntity addGradeAssessment(@RequestBody GradeAssessment gradeAssessment) {
+        gradeAssessmentService.addGradeAssessment(gradeAssessment);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
