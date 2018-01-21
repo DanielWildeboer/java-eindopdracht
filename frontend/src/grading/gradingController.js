f26d2d448490f8fb10e8c01a55f988d33c646217
Pim Gelmers <p.gelmers@live.nl>
2018-01-21 21:24:25 +0100

    Bjorne

--- frontend/src/grading/gradingController.js
+++ frontend/src/grading/gradingController.js
@@ -1,14 +1,7 @@
 var app = angular.module('gradeApp', ['ngRoute', 'ngMaterial']);
 app.controller('gradingController', function ($scope, $location, gradingService) {
-   $scope.gradeAssessment = [];
-
-    $scope.grades = {
-        grade: "7.0",
-        names: ["Bjorne Hoeksema", "Lesley van Oostenrijk", "Pim Gelmers", "Daniel Wildeboer", "Rik Hassing", "Ramon Valk"]
-    };
-
-    $scope.lol = gradingService.getGradeAssessment()
-        .then(function(response){
+    gradingService.getGradeAssessment(0)
+        .then(function (response) {
             $scope.gradeAssessment = response;
         });
 });
--- frontend/src/grading/gradingService.js
+++ frontend/src/grading/gradingService.js
@@ -1,8 +1,8 @@
 app.service('gradingService', function ($http) {
-    this.getGradeAssessment = function() {
+    this.getGradeAssessment = function(groupId) {
         return $http({
             method: "GET",
-            url: "http://127.0.0.1:8080/api/group/0"
+            url: "http://127.0.0.1:8080/api/group/" + groupId
         }).then(function(response){
             return response.data;
         });
