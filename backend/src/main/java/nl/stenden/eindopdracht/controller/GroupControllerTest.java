4fe8694fa0bc55f56790df0c399c5806556dcb0f
Ramon Valk <valkramon1@gmail.com>
2018-01-24 13:46:01 +0100

    Added Test class doesn't work sadly

---
+++ backend/src/main/java/nl/stenden/eindopdracht/controller/GroupControllerTest.java
@@ -0,0 +1,27 @@
+package nl.stenden.eindopdracht.controller;
+
+import nl.stenden.eindopdracht.Application;
+import nl.stenden.eindopdracht.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+// ApplicationContext will be loaded from "classpath:/com/example/OrderServiceTest-context.xml"
+@ContextConfiguration
+public class GroupControllerTest {
+
+    @Autowired
+    private Application App;
+
+    @Test
+    public void testApplication() {
+        assertThat(App).isNotNull();
+    }
+}
