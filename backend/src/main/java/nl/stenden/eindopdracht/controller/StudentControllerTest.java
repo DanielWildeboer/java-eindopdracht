0179f0f1fc3d6642f9929b45507575e2c9bdad7d
Bjorne Hoeksema <bjorne.hoeksema@student.stenden.com>
2018-01-24 11:33:52 +0100

    Init test

---
+++ backend/src/main/java/nl/stenden/eindopdracht/controller/StudentControllerTest.java
@@ -0,0 +1,33 @@
+package nl.stenden.eindopdracht.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+    @Test
+    void students() {
+        System.out.println("1");
+    }
+
+    @Test
+    void student() {
+        System.out.println("2");
+    }
+
+    @Test
+    void addStudent() {
+        System.out.println("3");
+    }
+
+    @Test
+    void updateStudent() {
+        System.out.println("4");
+    }
+
+    @Test
+    void removeStudent() {
+        System.out.println("5");
+    }
+
+}
